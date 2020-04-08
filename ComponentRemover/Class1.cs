a8d8079aec026679cd341fed0e54e65ac7595170
Winston Lowe <wel97459@gmail.com>
2020-04-08 12:37:12 -0700

    Adding Project Coded.

---
+++ ComponentRemover/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BaseX;
+using FrooxEngine;
+using FrooxEngine.LogiX;
+
+namespace ComponentRemover
+{
+    [Category("Util")]
+    public class ComponentRemover : Component
+    {
+        public readonly Sync<bool> HasRun;
+        protected override void OnAwake()
+        {
+            base.OnAwake();
+        }
+
+        protected override void OnCommonUpdate()
+        {
+            if (!HasRun.Value)
+            {
+                var list = Slot.GetComponentsInChildren<LogixReference>();
+                list.ForEach(lRef => lRef.Destroy(sendDestroyingEvent: true));
+             }
+            HasRun.Value = true;
+
+        }
+    }
+}
---
+++ ComponentRemover/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ComponentRemover")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ComponentRemover")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7d3899da-38f4-4ebc-81b7-a67c918ad9cd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
